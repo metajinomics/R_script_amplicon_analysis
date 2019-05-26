5c4bd81a77bb6dc6cf879069265929e4bb936823
Jin(Jinlyung) Choi <jinchoi721@gmail.com>
2019-05-25 17:00:53 -0700

    Create mothur_phyloseq.R

---
+++ mothur_phyloseq.R
@@ -0,0 +1,3 @@
+library(phyloseq)
+
+mothur_data <- import_mothur(mothur_shared_file = "stability.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.opti_mcc.unique_list.shared", mothur_constaxonomy_file = "stability.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.opti_mcc.unique_list.0.03.cons.taxonomy")
